c34ffdfa73ddf5d1982f368b1e30364b17960f07
Jesse Luehrs <doy@tozt.net>
2009-12-19 11:36:24 -0600

    initial commit

---
+++ lib/MooseX/Module/Refresh.pm
@@ -0,0 +1,71 @@
+package MooseX::Module::Refresh;
+use Moose;
+
+=head1 NAME
+
+MooseX::Module::Refresh -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-moosex-module-refresh at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-Module-Refresh>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc MooseX::Module::Refresh
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MooseX-Module-Refresh>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MooseX-Module-Refresh>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-Module-Refresh>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MooseX-Module-Refresh>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
